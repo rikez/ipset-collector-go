ac8e415a31acb80e4c82421297d56cd118a9cfda
Enrico Alvarenga <enricomalvarenga@gmail.com>
2019-07-04 01:37:19 -0300

    Initializing dep

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+
+}
